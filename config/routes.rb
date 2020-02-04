ebb02a8a61494bdaef275ed9ade803fead1136f2
Marcelo Escobar <escobar@br.systems>
2020-02-03 22:57:33 -0300

    updated asset_path on carrossel

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'main#index'
 
   get 'index', to: 'main#index'
-  get 'products', to: 'products#index'
+  # get 'products', to: 'products#index'
   get 'contact', to: 'main#index'
 end
